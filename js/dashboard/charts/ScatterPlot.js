937ee1f019f3a1b65f4994076899fd4c3a9bceb5
Alexandr Zelenin <zeleniy.spb@gmail.com>
2017-10-19 23:24:42 +0300

    Add scatter plot padding

--- js/dashboard/charts/ScatterPlot.js
+++ js/dashboard/charts/ScatterPlot.js
@@ -15,6 +15,12 @@ class ScatterPlot extends Widget {
     super(options);
 
     this._data = undefined;
+    this._margin = {
+      top: 0,
+      right: 10,
+      bottom: 20,
+      left: 0
+    };
   }
 
 
@@ -24,12 +30,11 @@ class ScatterPlot extends Widget {
    */
   getMargin() {
 
-    var maxR = Math.min(
-      this.getOuterWidth() - this._margin.left - this._margin.right,
-      this.getOuterHeight() - this._margin.top - this._margin.bottom
-    ) / 10;
+    const margin = _.clone(super.getMargin());
 
-    return _.transform(super.getMargin(), (r, v, k) => r[k] = v + maxR);
+    margin.left += this._yAxisContainer.node().getBoundingClientRect().width;
+
+    return margin;
   }
 
 
@@ -42,7 +47,7 @@ class ScatterPlot extends Widget {
     if (this._data === undefined) {
       this._data = d3.range(10).map(function() {
         return {
-          x: Math.random() * 100000000,
+          x: Math.random(),
           y: Math.random(),
           value: Math.random()
         }
@@ -91,32 +96,46 @@ class ScatterPlot extends Widget {
       .attr('width', this.getOuterWidth())
       .attr('height', this.getOuterHeight());
 
-    const margin = this.getMargin();
-
-    this._canvas
-      .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+    var margin = this.getMargin();
 
     const data = this.getData();
 
     const innerWidth = this.getInnerWidth();
     const innerHeight = this.getInnerHeight();
 
-    var maxR = Math.min(innerWidth, innerHeight) / 10;
+    const maxR = Math.min(innerWidth, innerHeight) / 10;
 
-    var valueScale = d3.scaleLinear()
-      .range([5, maxR])
-      .domain([0, d3.max(data, d => d.y)]);
-
-    var xScale = d3.scaleLinear()
-      .range([0, innerWidth])
-      .domain(d3.extent(data, d => d.x));
-
-    var yScale = d3.scaleLinear()
+    const yDomain = d3.extent(data, d => d.y);
+    const yScale = d3.scaleLinear()
       .range([innerHeight, 0])
-      .domain([0, d3.max(data, d => d.y)]);
+      .domain(yDomain);
+
+    const yExtra = yScale.invert(innerHeight - maxR) * 1.2;
+    yScale.domain([yDomain[0] - yExtra, yDomain[1] + yExtra])
+
+    const yAxis = d3.axisLeft(yScale)
+    this._yAxisContainer
+      .call(d3.axisLeft(yScale));
+
+    YTicks.getInstance(yAxis, this._yAxisContainer)
+      .rarefy();
+
+    margin = this.getMargin();
+
+    const rScale = d3.scaleLinear()
+      .range([5, maxR])
+      .domain([0, d3.max(data, d => d.value)]);
+
+    const xDomain = d3.extent(data, d => d.x);
+    const xScale = d3.scaleLinear()
+      .range([0, this.getInnerWidth()])
+      .domain(xDomain);
+
+    const xExtra = xScale.invert(maxR) * 1.2;
+    xScale.domain([xDomain[0] - xExtra, xDomain[1] + xExtra])
 
     this._dots
-      .attr('r', d => valueScale(d.value))
+      .attr('r', d => rScale(d.value))
       .attr('cx', d => xScale(d.x))
       .attr('cy', d => yScale(d.y));
 
@@ -129,12 +148,8 @@ class ScatterPlot extends Widget {
     XTicks.getInstance(xAxis, this._xAxisContainer)
       .rarefy();
 
-    const yAxis = d3.axisLeft(yScale)
-    this._yAxisContainer
-      .call(d3.axisLeft(yScale));
-
-    YTicks.getInstance(yAxis, this._yAxisContainer)
-      .rarefy();
+    this._canvas
+      .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
   }
 
 
