f55a03afe212090e21307a93f3c26de8289a937d
Alexandr Zelenin <zeleniy.spb@gmail.com>
2017-12-22 15:12:06 +0300

    Change "ValueColumn" filter's value on mode change

--- js/angular-dashboard/Components/filter/filter.js
+++ js/angular-dashboard/Components/filter/filter.js
@@ -2,11 +2,24 @@ dashboardApp.component('filter', {
   bindings: {
     accessor: '<',
     value: '<',
+    mode: '<',
     onClick: '&'
   },
   controllerAs: '$ctrl',
   templateUrl: 'js/angular-dashboard/Components/filter/filter.html',
   controller: function() {
 
+
+    /**
+     * @returns {String}
+     */
+    this.getValue = function() {
+
+      if (this.accessor == 'ValueColumn') {
+        return this.value.replace(/(Case|Count|Size)$/, this.mode);
+      } else {
+        return this.value;
+      }
+    }
   }
 });
